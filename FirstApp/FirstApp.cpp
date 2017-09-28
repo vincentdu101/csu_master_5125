1ff488fe08868673c9631b7cf7904c6814daed66
Vincent Du <vincentdu101@gmail.com>
2017-09-27 23:47:55 -0700

    progress on tetris shapes

--- FirstApp/FirstApp.cpp
+++ FirstApp/FirstApp.cpp
@@ -2,11 +2,11 @@
 
 #include "Angel.h"
 
-const int NumPoints = 6;
+const int NumPoints = 8;
 GLuint program = 0;
-float spaceBuffer = 0.1;
-float startingXPoint;
-float startingYPoint;
+float spaceBuffer = 0.0;
+float startingXPoint = 0.0;
+float startingYPoint = 0.0;
 vec3 points[NumPoints];
 vec3 colors[NumPoints];
 int colorIndex = 0;
@@ -17,6 +17,8 @@ vec3 colorOptions[] = {
 	vec3(1.0, 0.0, 0.0),
 	vec3(0.0, 1.0, 0.0),
 	vec3(0.0, 0.0, 1.0),
+	vec3(1.0, 1.0, 0.0),
+	vec3(0.0, 1.0, 1.0),
 	vec3(1.0, 1.0, 0.0),
 	vec3(0.0, 1.0, 1.0),
 	vec3(1.0, 0.0, 1.0)
@@ -32,10 +34,12 @@ void setupColor(vec3 color) {
 	colors[0] = color;
 	colors[1] = colors[0];
 	colors[2] = colors[0];
-
 	colors[3] = colors[0];
+
 	colors[4] = colors[0];
 	colors[5] = colors[0];
+	colors[6] = colors[0];
+	colors[7] = colors[0];
 
 	//glBufferData( GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW );
 	glBufferData(GL_ARRAY_BUFFER, sizeof(points) + sizeof(colors), NULL, GL_STATIC_DRAW);
@@ -47,30 +51,30 @@ void
 init(void)
 {
 	// Specifiy the vertices for a triangle
+	float unit = 1.0 / 5;
+
 	vec3 vertices[] = {
-		vec3(-1.0 / 20, -1.0 / 20, 0.0),
-		vec3(0.0,  1.0 / 20, 0.0),
-		vec3(1.0 / 20, -1.0 / 20, 0.0)
+		vec3(unit, unit, 0.0),
+		vec3(unit, -unit, 0.0),
+		vec3(-unit, unit, 0.0),
+		vec3(-unit, unit, 0.0),
+		vec3(-unit, -unit, 0.0),
+		vec3(unit, -unit, 0.0),
 	};
 
 	// Select an arbitrary initial point inside of the triangle
 	points[0] = vec3(0.0, 0.0, 0.0);
 
 	// compute and store NumPoints - 1 new points
-	for (int i = 1; i < NumPoints; ++i) {
-		int j = rand() % 3;   // pick a vertex from the triangle at random
-
-		// Compute the point halfway between the selected vertex
-		//   and the previous point
-		points[i] = (points[i - 1] + vertices[j]) / 2.0;
-	}
 	points[0] = vertices[0];
 	points[1] = vertices[1];
 	points[2] = vertices[2];
+	points[3] = vertices[3];
 
-	points[3] = vertices[0] + vec3(0.0, 0.5, 0.0);
-	points[4] = vertices[1] + vec3(0.0, 0.5, 0.0);
-	points[5] = vertices[2] + vec3(0.0, 0.5, 0.0);
+	points[4] = vertices[0] + vec3(0.0, 0.5, 0.0);
+	points[5] = vertices[1] + vec3(0.0, 0.5, 0.0);
+	points[6] = vertices[2] + vec3(0.0, 0.5, 0.0);
+	points[7] = vertices[3] + vec3(0.0, 0.5, 0.0);
 
 	// Create a vertex array object
 	GLuint vao;
@@ -90,12 +94,12 @@ init(void)
 	// Initialize the vertex position attribute from the vertex shader
 	GLuint loc = glGetAttribLocation(program, "vPosition");
 	glEnableVertexAttribArray(loc);
-	glVertexAttribPointer(loc, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+	glVertexAttribPointer(loc, 6, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
 
 	// Initialize the vertex color attribute from the vertex shader
 	GLuint loc1 = glGetAttribLocation(program, "vColor");
 	glEnableVertexAttribArray(loc1);
-	glVertexAttribPointer(loc1, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(points)));
+	glVertexAttribPointer(loc1, 6, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(points)));
 
 	glClearColor(0.5, 0.5, 0.5, 1.0); // gray background
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
@@ -129,102 +133,23 @@ float generateTrianglesConsecutively(float startingXPoint, float startingYPoint,
 	indexUniform = glGetUniformLocation(program, "colorIndex");
 	float directionalBuffer = positive ? spaceBuffer : -spaceBuffer;
 
+	startingXPoint += directionalBuffer;
+	startingYPoint += directionalBuffer;
+
 	GLuint loc1 = glGetAttribLocation(program, "vColor");
 	glEnableVertexAttribArray(loc1);
-	glVertexAttribPointer(loc1, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(points)));
+	glVertexAttribPointer(loc1, 6, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(points)));
 
-	for (int i = 0; i < number; i++) {
-		if (direction == 'x') {
-			startingXPoint = startingXPoint + directionalBuffer;
-		}
-		else {
-			startingYPoint = startingYPoint + directionalBuffer;
-		}
-		colorAltIndex = colorAltIndex == -1 ? -1 : colorAltIndex == 5 ? 0 : colorAltIndex + 1;
-		colorIndex = colorIndex == 5 ? 0 : colorIndex + 1;
-		setupColor(colorOptions[colorIndex]);
-		addTriangle(startingXPoint, startingYPoint, xOffsetParam, yOffsetParam, indexUniform);
-	}
+	setupColor(colorOptions[0]);
+	addTriangle(startingXPoint, startingYPoint, xOffsetParam, yOffsetParam, indexUniform);
 	return direction == 'x' ? startingXPoint : startingYPoint;
-}
-
-void makeOuterWalls() {
-	startingXPoint = -0.95;
-	startingYPoint = -1.05;
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 20, 'y', true);
-	startingXPoint += spaceBuffer;
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 18, 'x', true);
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 8, 'y', false);
-	startingYPoint -= spaceBuffer * 1;
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 8, 'y', false);
-	startingYPoint -= spaceBuffer * 2;
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 18, 'x', false);
-}
-
-void makeLeftQuadrant() {
-	startingXPoint = (spaceBuffer * 2) - 0.95;
-	startingYPoint = (spaceBuffer * 20) - 1.05;
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 3, 'y', false);
-	startingYPoint -= spaceBuffer;
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 3, 'y', false);
-	startingYPoint -= spaceBuffer * 2;
-	startingXPoint += spaceBuffer;
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 1, 'x', false);
-	startingXPoint -= spaceBuffer * 2;
-	startingYPoint += spaceBuffer * 2;
-	startingXPoint += spaceBuffer * 2;
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 5, 'x', true);
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 5, 'y', true);
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 3, 'x', false);
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 3, 'y', false);
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 1, 'x', true);
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 1, 'y', true);
-	startingYPoint += spaceBuffer * 2;
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 5, 'x', true);
-	startingYPoint -= spaceBuffer * 2;
-	startingXPoint += spaceBuffer;
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 2, 'x', false);
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 5, 'y', false);
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 5, 'x', false);
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 5, 'y', false);
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 3, 'y', false);
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 2, 'x', false);
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 6, 'y', true);
-	startingXPoint += spaceBuffer * 4;
-	startingYPoint += spaceBuffer;
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 8, 'y', false);
-	startingYPoint += spaceBuffer * 7;
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 5, 'x', true);
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 9, 'y', true);
-	startingXPoint += spaceBuffer;
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 6, 'x', true);
-	startingXPoint -= spaceBuffer * 5;
-	startingYPoint -= spaceBuffer;
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 14, 'y', false);
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 6, 'x', false);
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 4, 'y', true);
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 4, 'x', true);
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 2, 'y', false);
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 2, 'x', false);
-	startingXPoint += spaceBuffer * 6;
-	startingYPoint -= spaceBuffer * 3;
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 15, 'y', true);
-	startingYPoint -= spaceBuffer * 6;
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 5, 'y', true);
-	startingXPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 1, 'x', true);
-	startingYPoint -= spaceBuffer * 2;
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 5, 'y', false);
-	startingYPoint -= spaceBuffer * 2;
-	startingXPoint += spaceBuffer * 2;
-	startingYPoint = generateTrianglesConsecutively(startingXPoint, startingYPoint, 5, 'y', true);
 }
 
 void
 display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);				// clear the window
-	makeOuterWalls();
-	makeLeftQuadrant();
+	generateTrianglesConsecutively(startingXPoint, startingYPoint, 0, 'y', true);
 	colorIndex = 0;								// need to reset otherwise colors keep changing
 	colorAltIndex = colorAltIndex == -1 ? -1 : 0;
 	glFlush();									// flush the buffer
